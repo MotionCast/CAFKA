6c1fc1ec899ef2cd4636f421ffc60a537ebb2ece
jacobszpz <jacobszpz@protonmail.com>
2023-03-03 21:59:30 +0000

    Add buttons module

---
+++ configuration/common/buttons_def.h
@@ -0,0 +1,8 @@
+#include <caf/gpio_pins.h>
+
+static const struct gpio_pin col[] = {
+        { .port = 0, .pin = 31 },
+        { .port = 0, .pin = 24 },
+};
+
+static const struct gpio_pin row[] = {};
