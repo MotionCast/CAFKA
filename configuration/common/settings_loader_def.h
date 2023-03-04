95bd75ed7e58bdead59376f6ffae0637d771fcec
jacobszpz <jacobszpz@protonmail.com>
2023-03-04 22:22:10 +0000

    Fix settings loader to load in the correct order and add backend
    Added necessary settings to enable NVS backend for the settings
    module. Settings loader will now load before BLE advertising and
    BLE bond.

--- configuration/common/settings_loader_def.h
+++ configuration/common/settings_loader_def.h
@@ -3,10 +3,7 @@
 static inline void get_req_modules(struct module_flags *mf)
 {
 	module_flags_set_bit(mf, MODULE_IDX(main));
-#ifdef CONFIG_CAF_BLE_ADV
-	module_flags_set_bit(mf, MODULE_IDX(ble_adv));
-#endif
-#ifdef CONFIG_CAF_BLE_STATE
-	module_flags_set_bit(mf, MODULE_IDX(ble_state));
-#endif
+	#ifdef CONFIG_CAF_BLE_STATE
+		module_flags_set_bit(mf, MODULE_IDX(ble_state));
+	#endif
 };
